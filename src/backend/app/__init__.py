089eaaac38f0dc8152d50c67e4c9f36e20985672
Voynov Alexey <112628165+LKolkovich@users.noreply.github.com>
2025-04-19 10:22:31 -0400

    edit models

--- src/backend/app/__init__.py
+++ src/backend/app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from .config import config_by_name
+from neomodel import config as neomodel_config
 
 
 def create_app(config_name='development'):
     app = Flask(__name__)
     app.config.from_object(config_by_name[config_name])
+    neomodel_config.DATABASE_URL = app.config['DATABASE_URL']
 
     return app
--- src/backend/app/config.py
+++ src/backend/app/config.py
@@ -5,9 +5,13 @@ class BaseConfig:
     DEBUG = False
     TESTING = False
     SECRET_KEY = os.environ.get("SECRET_KEY", "devkey")
-    NEO4J_URL = os.environ.get("NEO4J_URL", "bolt://neo4j:7687")
+
+    NEO4J_HOST = os.environ.get("NEO4J_HOST", "localhost")
+    NEO4J_PORT = os.environ.get("NEO4J_PORT", "7687")
     NEO4J_USERNAME = os.environ.get("NEO4J_USERNAME", "neo4j")
     NEO4J_PASSWORD = os.environ.get("NEO4J_PASSWORD", "password")
+
+    DATABASE_URL = f"bolt://{NEO4J_USERNAME}:{NEO4J_PASSWORD}@{NEO4J_HOST}:{NEO4J_PORT}"
 
 
 class DevelopmentConfig(BaseConfig):
