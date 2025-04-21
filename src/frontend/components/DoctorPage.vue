ab1494bb1c9629092815754b54729d3c8efb9dbd
Mariya Perebeynova <112629090+CatTichon@users.noreply.github.com>
2025-04-22 00:11:11 +0300

    Create DoctorPage.vue

---
+++ src/frontend/components/DoctorPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Header />
+    <h1>Страница доктора</h1>
+  </div>
+</template>
+
+<script>
+import Doctor from '../models/Doctor.js';
+import { getDoctorById } from '../api/entity.js';
+import Header from './Header.vue';
+
+export default {
+  components: { Header }
+}
+</script>
