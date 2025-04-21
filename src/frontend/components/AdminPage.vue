8469acbdd8a7f4b5e0c33480ca17391677d06f43
Mariya Perebeynova <112629090+CatTichon@users.noreply.github.com>
2025-04-22 00:11:25 +0300

    Add files via upload

---
+++ src/frontend/components/AdminPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Header />
+    <h1>Страница администратора</h1>
+  </div>
+</template>
+
+<script>
+import Header from './Header.vue';
+
+export default {
+  components: { Header }
+}
+</script>
