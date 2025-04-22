e536545c8e504e514ac4d92b7ef4e416b361acfa
Mariya Perebeynova <112629090+CatTichon@users.noreply.github.com>
2025-04-22 21:47:21 +0300

    Update index.js

--- src/frontend/router/index.js
+++ src/frontend/router/index.js
@@ -1,19 +1,28 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import LoginPage from '../components/LoginPage.vue';
-import DoctorPage from '../components/DoctorPage.vue';
-import AdminPage from '../components/AdminPage.vue';
-import UserPage from '../components/UserPage.vue';
+import { createRouter, createWebHistory } from 'vue-router'
+import LoginPage from '../components/LoginPage.vue'
+import DoctorPage from '../components/DoctorPage.vue'
+import AdminPage from '../components/AdminPage.vue'
+import AdminPageBranch from '@/components/AdminPageBranch.vue'
+import AdminPageReport from '@/components/AdminPageReport.vue'
+import AdminPageServices from '@/components/AdminPageServices.vue'
+import AdminTables from '@/components/AdminTables.vue'
+import UserPage from '../components/UserPage.vue'
+import AdminPageUsers from "@/components/AdminPageUsers.vue";
 
 const routes = [
-    { path: '/', name: 'Login', component: LoginPage },
-    { path: '/doctor', name: 'DoctorPage', component: DoctorPage },
-    { path: '/admin', name: 'AdminPage', component: AdminPage },
-    { path: '/user', name: 'UserPage', component: UserPage },
-];
+    { path: '/', component: LoginPage },
+    { path: '/doctor', component: DoctorPage },
+    { path: '/admin', component: AdminPage },
+    { path: '/admin/User', component: AdminPageUsers },
+    { path: '/admin/Procedure', component: AdminPageServices },
+    { path: '/admin/Office', component: AdminPageBranch },
+    { path: '/admin/Appointment', component: AdminPageReport },
+    { path: '/user', component: UserPage }
+]
 
 const router = createRouter({
     history: createWebHistory(),
-    routes,
-});
+    routes
+})
 
-export default router;
+export default router
