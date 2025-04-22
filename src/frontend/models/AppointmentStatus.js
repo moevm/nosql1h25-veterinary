e429a42a94fdb2a251339ef91ac081ac895f404c
Mariya Perebeynova <112629090+CatTichon@users.noreply.github.com>
2025-04-22 22:38:45 +0300

    Delete src/frontend directory

--- src/frontend/models/AppointmentStatus.js
+++
@@ -1,7 +0,0 @@
-export const AppointmentStatus = {
-    PENDING: 'ожидает подтверждения',
-    CONFIRMED: 'подтвержден',
-    CANCELED: 'отменен',
-    COMPLETED: 'проведен'
-};
-
--- src/frontend/models/Doctor.js
+++
@@ -1,9 +0,0 @@
-import { User } from './User.js';
-
-export class Doctor extends User {
-    constructor(second_name, first_name, last_name, login, password_hash, birth_date, email, gender, phone_number, role, experience, specialization) {
-        super(second_name, first_name, last_name, login, password_hash, birth_date, email, gender, phone_number, role);
-        this.experience = experience;
-        this.specialization = specialization;
-    }
-}
