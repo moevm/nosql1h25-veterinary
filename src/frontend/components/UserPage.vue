dd7b23b15afe0f78e6983585d740a90a4ef2916a
Хидда Абдулла <70700738+aboudhiddah@users.noreply.github.com>
2025-04-22 03:18:58 +0300

    Update UserPage.vue

--- src/frontend/components/UserPage.vue
+++ src/frontend/components/UserPage.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
     <Header />
-    <h1>Страница пользователя (пациента)</h1>
+    <UserTables />
   </div>
 </template>
 
 <script>
 import Header from './Header.vue';
+import UserTables from './UserTables.vue';
 
 export default {
-  components: { Header }
+  components: { Header, UserTables }
 }
 </script>
