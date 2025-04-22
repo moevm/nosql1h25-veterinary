66a99317fccb12ee5a8d99c272775571e7c38b8d
Mariya Perebeynova <112629090+CatTichon@users.noreply.github.com>
2025-04-22 21:43:12 +0300

    Add files via upload

---
+++ src/frontend/components/AdminPageUsers.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Header />
+    <AdminTables />
+  </div>
+</template>
+
+
+<script>
+import HeaderAdmin from './HeaderAdmin.vue';
+import AdminTables from './AdminTables.vue';
+
+export default {
+  components: { Header: HeaderAdmin, AdminTables }
+}
+</script>
