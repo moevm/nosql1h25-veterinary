36c1fb2545390947ca59b58cd9bd79f00617d6c5
Mariya Perebeynova <112629090+CatTichon@users.noreply.github.com>
2025-04-22 22:37:16 +0300

    Add files via upload

---
+++ src/frontend/components/UserPageMakeAppointment.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Header />
+    <h1> Запись на приём </h1>
+    <UserTable />
+  </div>
+</template>
+
+<script>
+import HeaderUser from './HeaderUser.vue';
+import UserTables from './UserTables.vue';
+
+export default {
+  name: 'UserPage',
+  components: {
+    Header: HeaderUser, UserTables: UserTables
+  }
+}
+</script>
---
+++ src/frontend/components/UserPageАppointments.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Header />
+    <h1> Мои приёмы </h1>
+    <UserTable />
+  </div>
+</template>
+
+<script>
+import HeaderUser from './HeaderUser.vue';
+import UserTables from './UserTables.vue';
+
+export default {
+  name: 'UserPage',
+  components: {
+    Header: HeaderUser, UserTables: UserTables
+  }
+}
+</script>
