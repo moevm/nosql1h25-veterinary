a057239d09c58d891cdaa2b08e97a84be6895ef8
Mariya Perebeynova <112629090+CatTichon@users.noreply.github.com>
2025-04-22 16:31:26 +0300

    Update vite.config.js

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path' // ← добавь это
 
 export default defineConfig({
     plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './') // указывает на корень frontend/
+        }
+    },
     server: {
         port: 3000
     }
