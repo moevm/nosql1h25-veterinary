45399b025147f134728944c0097782523e834126
Mariya Perebeynova <112629090+CatTichon@users.noreply.github.com>
2025-04-22 16:30:54 +0300

    Update DoctorPage.vue

--- src/frontend/components/DoctorPage.vue
+++ src/frontend/components/DoctorPage.vue
@@ -1,16 +1,19 @@
-<template>
-  <div>
-    <Header />
-    <DoctorTables />
-  </div>
-</template>
-
 <script>
+import {Doctor} from '@/models/Doctor';
+import { getDoctorById } from '@/api/entity';
 import Header from './Header.vue';
-import DoctorTables from './DoctorTables.vue';
 
-  
 export default {
-  components: { Header, DoctorTables }
+  components: { Header },
+  data() {
+    return {
+      doctor: null
+    }
+  },
+  async mounted() {
+    // Временный пример — загружаем доктора с id 1
+    const data = await getDoctorById(1)
+    this.doctor = new Doctor(data)
+  }
 }
 </script>
