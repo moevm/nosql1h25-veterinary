15a66e25d447e96cec757af5e1c34d7a1a10e45b
Mariya Perebeynova <112629090+CatTichon@users.noreply.github.com>
2025-04-22 22:50:24 +0300

    Update index.js

--- src/frontend/router/index.js
+++ src/frontend/router/index.js
@@ -4,6 +4,9 @@ import DoctorPage from '../components/DoctorPage.vue'
 import AdminPageBranch from '@/components/AdminPageBranch.vue'
 import AdminPageReport from '@/components/AdminPageReport.vue'
 import AdminPageServices from '@/components/AdminPageServices.vue'
+import UserPageGetPet from "@/components/UserPageGetPet.vue";
+import UserPageMakeAppointment from "@/components/UserPageMakeAppointment.vue";
+import UserPageAppointments from "@/components/UserPageАppointments.vue";
 import UserPage from '../components/UserPage.vue'
 import AdminPageUsers from "@/components/AdminPageUsers.vue";
 import AdminPagePet from "@/components/AdminPagePet.vue";
@@ -13,6 +16,10 @@ const routes = [
     { path: '/doctor', component: DoctorPage },
     { path: '/admin',  redirect: '/admin/User' },
     { path: '/admin/User', component: AdminPageUsers },
+    { path: '/admin/Pet', component: UserPageGetPet },
+    { path: '/admin/Bookings', component: UserPageMakeAppointment },
+    { path: '/admin/AppointmentUser', component: UserPageAppointments },
+
     { path: '/admin/Procedure', component: AdminPageServices },
     { path: '/admin/Office', component: AdminPageBranch },
     { path: '/admin/Appointment', component: AdminPageReport },
